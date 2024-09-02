836eee7092f146fda80d41679a284109600122f1
chandusiriyala <chandusiriyala116@gmail.com>
2024-09-02 18:39:57 +0530

    Added Email Feature

--- components/Typeform.tsx
+++ components/Typeform.tsx
@@ -46,6 +46,9 @@
     const [firstPage, setFirstPage] = useState(true);
 
     const [formVisible, setFormVisible] = useState(false); // Track form visibility
+    const [score, setScore] = useState(0);
+    const [performanceMessage, setPerformanceMessage] = useState('');
+    const [scoreColor, setScoreColor] = useState('');
 
     // Update form visibility based on conditions
     useEffect(() => {
@@ -117,78 +120,107 @@
       }
     };
 
-    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    async function handleSubmit(e) {
       e.preventDefault();
       setSubmit(true);
+    
+      if (emailError !== '') {
+        return;
+      }
+    
 
-      // Check if there are any validation errors
-  if (emailError !== '') {
-    return;
-  }
+      const labeledData = {};
+      questions.forEach((question) => {
+        labeledData[question.label] = formData[question.id];
+      });
 
+    
+    
+      let newScore = 0;
 
-      let formDataEncoded = new URLSearchParams();
-      formDataEncoded.append('entry.1374464205', formData['entry.1374464205']);
-      formDataEncoded.append('entry.569871843', formData['entry.569871843']);
-      formDataEncoded.append('entry.26494061', formData['entry.26494061']);
-      formDataEncoded.append('entry.135519665', formData['entry.135519665']);
-      // new
-      formDataEncoded.append('entry.1727644052', formData['entry.1727644052']);
-      formDataEncoded.append('entry.1227823523', formData['entry.1227823523']);
-      formDataEncoded.append('entry.937292675', formData['entry.937292675']);
-      formDataEncoded.append('entry.776793653', formData['entry.776793653']);
-      formDataEncoded.append('entry.1169714985', formData['entry.1169714985']);
-      formDataEncoded.append('entry.935255613', formData['entry.935255613']);
-      formDataEncoded.append('entry.385599653', formData['entry.385599653']);
-      formDataEncoded.append('entry.54283470', formData['entry.54283470']);
-      formDataEncoded.append('entry.772817465', formData['entry.772817465']);
-      formDataEncoded.append('entry.2001338899', formData['entry.2001338899']);
-      formDataEncoded.append('entry.1383095573', formData['entry.1383095573']);
-      formDataEncoded.append('entry.1372802332', formData['entry.1372802332']);
-      formDataEncoded.append('entry.2056251646', formData['entry.2056251646']);
-      formDataEncoded.append('entry.224539324', formData['entry.224539324']);
-      formDataEncoded.append('entry.758053870', formData['entry.758053870']);
-      formDataEncoded.append('entry.895823797', formData['entry.895823797']);
-      formDataEncoded.append('entry.51915641', formData['entry.51915641']);
-      formDataEncoded.append('entry.349670037', formData['entry.349670037']);
-      formDataEncoded.append('entry.79931368', formData['entry.79931368']);
-      formDataEncoded.append('entry.474857398', formData['entry.474857398']);
-      formDataEncoded.append('entry.1317914009', formData['entry.1317914009']);
-
-      formDataEncoded.append('entry.458840601', formData['entry.458840601']);
-      formDataEncoded.append('entry.244541044', formData['entry.244541044']);
-      formDataEncoded.append('entry.224739269', formData['entry.224739269']);
-      formDataEncoded.append('entry.637530406', formData['entry.637530406']);
-
-      try {
-        const res = await fetch(
-          'https://docs.google.com/forms/u/0/d/e/1FAIpQLSffa3uWVXkwmcP_SH19lIz_A0buWGAJ0jzJi_Yo-6zMkQQHlA/formResponse',
-          {
-            method: 'POST',
-            mode: 'no-cors',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: formDataEncoded.toString(),
+      Object.entries(formData).forEach(([key, answer]) => {
+        const lowerCaseAnswer = answer.toLowerCase();
+      
+        if (lowerCaseAnswer === 'yes') {
+          newScore += 10;
+        }
+      
+        // Scoring logic for Question 12 (assuming the ID for Question 12 is 'entry.1234567890')
+        if (key === 'entry.54283470') { 
+          if (lowerCaseAnswer === 'less than 5%') {
+            newScore += 2.5;
+          } else if (lowerCaseAnswer === '5% to 10%') {
+            newScore += 5;
+          } else if (lowerCaseAnswer === '10-20%') {
+            newScore += 7.5;
+          } else if (lowerCaseAnswer === 'more than 20%') {
+            newScore += 10;
           }
-        );
-        if (res.ok) {
-          // Handle success
-          console.log('Form submitted successfully');
+        }
+      
+        // Scoring logic for Questions 13 and 14
+        // Replace 'entry.9876543210' and 'entry.1122334455' with the actual IDs for these questions
+        if (key === 'entry.2001338899' || key === 'entry.772817465') { 
+          if (lowerCaseAnswer === 'less than 5%') {
+            newScore += 2.5;
+          } else if (lowerCaseAnswer === '5% to 10%') {
+            newScore += 5;
+          } else if (lowerCaseAnswer === '10-20%') {
+            newScore += 7.5;
+          } else if (lowerCaseAnswer === 'more than 20%') {
+            newScore += 10;
+          }
+        }
+      });
+      
+      let performanceMessage = '';
+      let scoreColor = '';
+      
+      if (newScore < 100) {
+        performanceMessage = "The volunteering initiative at your company appears to be poor, with significant room for improvement. Please contact us for a detailed report highlighting areas of strength and areas needing improvement.";
+        scoreColor = '#FF0000'; // Red
+      } else if (newScore >= 100 && newScore < 200) {
+        performanceMessage = "The volunteering initiative at your company is average, but there is still room for improvement. Please find attached a detailed report outlining strengths and areas requiring attention.";
+        scoreColor = '#FFBF00'; // Amber
+      } else {
+        performanceMessage = "Kudos! Your company is performing well in the volunteering initiative. Feel free to reach out to discuss how we can further enhance your efforts.";
+        scoreColor = '#4CAF50'; // Green
+      }
+      
+      try {
+        const response = await fetch('/api/send-email', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(labeledData),
+        });
+    
+        if (response.ok) {
+          console.log('Email sent successfully!');
         } else {
-          // Handle error
-          console.error('Form submission failed');
+          console.error('Error sending email.');
+          console.log("Response status:", response);
         }
       } catch (error) {
-        console.error('Form submission failed:', error);
+        console.error('Error sending email:', error);
       }
+
+      setScore(newScore);
+      setPerformanceMessage(performanceMessage);
+      setScoreColor(scoreColor);
+      
+
+
+      
     }
+    
 
     const questions = [
       {
         id: 'entry.1374464205',
         label:
-          'Do you have an executive sponsor for the employee volunteering program within leadership ?',
+          '1.Do you have an executive sponsor for the employee volunteering program within leadership ?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -196,7 +228,7 @@
       },
       {
         id: 'entry.569871843',
-        label: 'Do you have a employee volunteering policy ?',
+        label: '2.Do you have a employee volunteering policy ?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -205,7 +237,7 @@
       {
         id: 'entry.26494061',
         label:
-          'Does your employee volunteering Policy that aligns with your CSR policy ?',
+          '3.Does your employee volunteering Policy that aligns with your CSR policy ?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -214,7 +246,7 @@
       {
         id: 'entry.135519665',
         label:
-          'Do you have matching grant policy / grant giving for employee volunteering program ?',
+          '4.Do you have matching grant policy / grant giving for employee volunteering program ?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -223,7 +255,7 @@
       // new
       {
         id: 'entry.1727644052',
-        label: 'Do you have any employee payroll deduction program?',
+        label: '5.Do you have any employee payroll deduction program?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -231,7 +263,7 @@
       },
       {
         id: 'entry.1227823523',
-        label: 'Do you have volunteering code of ethics defined?',
+        label: '6.Do you have volunteering code of ethics defined?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -240,7 +272,7 @@
       {
         id: 'entry.937292675',
         label:
-          'Does your leadership team invest time in employee volunteering program?',
+          '7.Does your leadership team invest time in employee volunteering program?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -249,7 +281,7 @@
       {
         id: 'entry.776793653',
         label:
-          'Do you publish volunteering impacts/ hours / metrics in ESG / Sustainability reports?',
+          '8.Do you publish volunteering impacts/ hours / metrics in ESG / Sustainability reports?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -258,7 +290,7 @@
       {
         id: 'entry.1169714985',
         label:
-          'Does your employee volunteering and philanthropy / CSR spend work in tandem?',
+          '9.Does your employee volunteering and philanthropy / CSR spend work in tandem?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -266,7 +298,7 @@
       },
       {
         id: 'entry.935255613',
-        label: 'Do you have volunteering time off?',
+        label: '10.Do you have volunteering time off?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -275,7 +307,7 @@
       {
         id: 'entry.385599653',
         label:
-          'Do you associate volunteering under performance review process of the employee?',
+          '11.Do you associate volunteering under performance review process of the employee?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -284,11 +316,10 @@
       {
         id: 'entry.54283470',
         label:
-          'What is the current % of employee volunteering against overall headcount?',
+          '12.What is the current % of employee volunteering against overall headcount?',
         options: [
           { value: 'More than 20%', label: 'More than 20%' },
           { value: '10-20%', label: '10-20%' },
-          { value: '20-30%', label: '20-30%' },
           { value: '5% to 10%', label: '5% to 10%' },
           { value: 'Less than 5%', label: 'Less than 5%' },
         ],
@@ -296,11 +327,10 @@
       {
         id: 'entry.772817465',
         label:
-          'What is the % of participation by Senior Leadership against overall volunteering ?',
+          '13.What is the % of participation by Senior Leadership against overall volunteering ?',
         options: [
           { value: 'More than 20%', label: 'More than 20%' },
           { value: '10-20%', label: '10-20%' },
-          { value: '20-30%', label: '20-30%' },
           { value: '5% to 10%', label: '5% to 10%' },
           { value: 'Less than 5%', label: 'Less than 5%' },
         ],
@@ -308,11 +338,10 @@
       {
         id: 'entry.2001338899',
         label:
-          'What is the % of volunteers return to volunteering more than 2 times ?',
+          '14.What is the % of volunteers return to volunteering more than 2 times ?',
         options: [
           { value: 'More than 20%', label: 'More than 20%' },
           { value: '10-20%', label: '10-20%' },
-          { value: '20-30%', label: '20-30%' },
           { value: '5% to 10%', label: '5% to 10%' },
           { value: 'Less than 5%', label: 'Less than 5%' },
         ],
@@ -320,7 +349,7 @@
       {
         id: 'entry.1383095573',
         label:
-          'Do you provide orientation to new volunteers at the beginning of every event?',
+          '15.Do you provide orientation to new volunteers at the beginning of every event?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -328,7 +357,7 @@
       },
       {
         id: 'entry.1372802332',
-        label: 'Do you collect feedback from volunteers after every program ?',
+        label: '16.Do you collect feedback from volunteers after every program ?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -337,7 +366,7 @@
       {
         id: 'entry.2056251646',
         label:
-          'Do you track the hours invested and provide volunteering certificate ?',
+          '17.Do you track the hours invested and provide volunteering certificate ?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -345,7 +374,7 @@
       },
       {
         id: 'entry.224539324',
-        label: 'Do you provide transportation for employee volunteering ?',
+        label: '18.Do you provide transportation for employee volunteering ?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -354,7 +383,7 @@
       {
         id: 'entry.758053870',
         label:
-          'Do you provide food and beverages for volunteers during volunteering events ?',
+          '19.Do you provide food and beverages for volunteers during volunteering events ?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -363,7 +392,7 @@
 
       {
         id: 'entry.895823797',
-        label: 'Do you have a volunteer Rewards and Recognition program ?',
+        label: '20.Do you have a volunteer Rewards and Recognition program ?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -372,7 +401,7 @@
       {
         id: 'entry.51915641',
         label:
-          'Are volunteers recognized on a regular frequency through Reward and Recognition program?',
+          '21.Are volunteers recognized on a regular frequency through Reward and Recognition program?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -381,7 +410,7 @@
       {
         id: 'entry.349670037',
         label:
-          'Do you recognize volunteers in front of their respective leadership ?',
+          '22.Do you recognize volunteers in front of their respective leadership ?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -390,7 +419,7 @@
       {
         id: 'entry.79931368',
         label:
-          'Do you share the stories of impact through volunteering internally and externally ?',
+          '23.Do you share the stories of impact through volunteering internally and externally ?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -398,7 +427,7 @@
       },
       {
         id: 'entry.474857398',
-        label: 'Do you measure the impact of a program executed by volunteers ?',
+        label: '24.Do you measure the impact of a program executed by volunteers ?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -407,7 +436,7 @@
       {
         id: 'entry.1317914009',
         label:
-          'Do you communicate the feedback / testimony from beneficiaries back to employees ?',
+          '25.Do you communicate the feedback / testimony from beneficiaries back to employees ?',
         options: [
           { value: 'Yes', label: 'Yes' },
           { value: 'No', label: 'No' },
@@ -416,22 +445,22 @@
 
       {
         id: 'entry.458840601',
-        label: 'Name of the person taking the Survey',
+        label: '26.Name of the person taking the Survey',
         inputType: 'text',
       },
       {
         id: 'entry.244541044',
-        label: 'Title / Designation of the person taking this survey',
+        label: '27.Title / Designation of the person taking this survey',
         inputType: 'text',
       },
       {
         id: 'entry.224739269',
-        label: 'Name of the Organization ',
+        label: '28.Name of the Organization ',
         inputType: 'text',
       },
       {
         id: 'entry.637530406',
-        label: 'Email address of the person taking the survey',
+        label: '29.Email address of the person taking the survey',
         inputType: 'email',
       },
     ];
@@ -465,15 +494,25 @@
               </div>
             )}
 
-            {submit && !firstPage && (
-              <div className="bg-cover h-[100vh] w-[100vw] gap-4 flex flex-col lg:flex-row justify-center items-center">
-                <div className="flex flex-col justify-center text-[18px] items-start w-[90vw] mx-auto">
-                  <h1 className="mx-auto">
-                    Thank You for your response, Check your mail for more details.
-                  </h1>
-                </div>
-              </div>
-            )}
+ 
+{submit && !firstPage && (
+  <div className="bg-cover h-[100vh] w-[100vw] gap-4 flex flex-col lg:flex-row justify-center items-center">
+    <div className="flex flex-col justify-center text-[18px] items-start w-[90vw] mx-auto">
+      <h1 className="mx-auto text-2xl font-bold text-white">
+        Thank You for your response! Check your mail for more details.
+      </h1>
+      <h2 className="mx-auto text-xl text-white mt-4">
+        Your Score: <span className="font-bold"  style={{ color: scoreColor }}>{score}</span>
+      </h2>
+      <div className="mt-4 p-4 rounded-md" style={{ backgroundColor: scoreColor }}>
+        <p className="text-center text-lg font-semibold text-white">{performanceMessage}</p>
+      </div>
+    </div>
+  </div>
+)}
+
+
+ 
 
             {!submit && !firstPage && (
               <form
